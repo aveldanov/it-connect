57d55ea979b3653916c193e3b8bf71d61ba5dc39
Anton Veldanov <anton.veldanov@gmail.com>
2019-03-24 18:08:38 -0700

    Redux

--- validation/register.js
+++ validation/register.js
@@ -28,13 +28,14 @@ module.exports = function validateRegisterInput(data) {
   if (Validator.isEmpty(data.email)) {
     errors.email = "Email field is required"
   }
+  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+    errors.password = 'Password must be between 6 and 30 characters';
+  }
   if (Validator.isEmpty(data.password)) {
     errors.password = "Password field is required"
   }
 
-  if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-    errors.password = 'Password must be between 6 and 30 characters';
-  }
+
 
   if (Validator.isEmpty(data.password2)) {
     errors.password2 = "Confirmed password field is required"
